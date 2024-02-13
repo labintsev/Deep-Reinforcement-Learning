68a26c723b4d72254dc873763809df1c4352dd43
Andrej <ailabintsev@fa.ru>
2024-02-13 11:08:59 +0300

    fix code, add requirements for Chapter04

--- Chapter04/01_cartpole.py
+++ Chapter04/01_cartpole.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import gym
+import gymnasium as gym
 from collections import namedtuple
 import numpy as np
 from tensorboardX import SummaryWriter
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-HIDDEN_SIZE = 128
+HIDDEN_SIZE = 64
 BATCH_SIZE = 16
 PERCENTILE = 70
 
@@ -35,14 +35,14 @@ def iterate_batches(env, net, batch_size):
     batch = []
     episode_reward = 0.0
     episode_steps = []
-    obs = env.reset()
+    obs, info = env.reset()
     sm = nn.Softmax(dim=1)
     while True:
         obs_v = torch.FloatTensor([obs])
         act_probs_v = sm(net(obs_v))
         act_probs = act_probs_v.data.numpy()[0]
         action = np.random.choice(len(act_probs), p=act_probs)
-        next_obs, reward, is_done, _ = env.step(action)
+        next_obs, reward, is_done, _, _ = env.step(action)
         episode_reward += reward
         step = EpisodeStep(observation=obs, action=action)
         episode_steps.append(step)
@@ -51,7 +51,7 @@ def iterate_batches(env, net, batch_size):
             batch.append(e)
             episode_reward = 0.0
             episode_steps = []
-            next_obs = env.reset()
+            next_obs, _ = env.reset()
             if len(batch) == batch_size:
                 yield batch
                 batch = []
