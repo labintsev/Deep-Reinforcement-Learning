a2812cb29c6a7056c8c5c1c646134fdb4ca6bcda
labintsev <andrej.labintsev@yandex.ru>
2024-03-06 01:31:41 +0300

    fix q learning

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import gym
+import gymnasium as gym
 import collections
 from tensorboardX import SummaryWriter
 
-ENV_NAME = "FrozenLake-v0"
+ENV_NAME = "FrozenLake-v1"
 GAMMA = 0.9
 ALPHA = 0.2
 TEST_EPISODES = 20
@@ -12,20 +12,23 @@ TEST_EPISODES = 20
 class Agent:
     def __init__(self):
         self.env = gym.make(ENV_NAME)
-        self.state = self.env.reset()
-        self.values = collections.defaultdict(float)
+        self.state, _ = self.env.reset()
+        self.Q_values = collections.defaultdict(float)
 
     def sample_env(self):
         action = self.env.action_space.sample()
         old_state = self.state
-        new_state, reward, is_done, _ = self.env.step(action)
-        self.state = self.env.reset() if is_done else new_state
+        new_state, reward, terminated, truncated, _ = self.env.step(action)
+        if terminated or truncated:
+            self.state, _ = self.env.reset()
+        else:
+            self.state = new_state
         return old_state, action, reward, new_state
 
     def best_value_and_action(self, state):
         best_value, best_action = None, None
         for action in range(self.env.action_space.n):
-            action_value = self.values[(state, action)]
+            action_value = self.Q_values[(state, action)]
             if best_value is None or best_value < action_value:
                 best_value = action_value
                 best_action = action
@@ -34,17 +37,17 @@ class Agent:
     def value_update(self, s, a, r, next_s):
         best_v, _ = self.best_value_and_action(next_s)
         new_v = r + GAMMA * best_v
-        old_v = self.values[(s, a)]
-        self.values[(s, a)] = old_v * (1-ALPHA) + new_v * ALPHA
+        old_v = self.Q_values[(s, a)]
+        self.Q_values[(s, a)] = old_v * (1 - ALPHA) + new_v * ALPHA
 
     def play_episode(self, env):
         total_reward = 0.0
-        state = env.reset()
+        state, _ = env.reset()
         while True:
             _, action = self.best_value_and_action(state)
-            new_state, reward, is_done, _ = env.step(action)
+            new_state, reward, terminated, truncated, _ = env.step(action)
             total_reward += reward
-            if is_done:
+            if terminated or truncated:
                 break
             state = new_state
         return total_reward
