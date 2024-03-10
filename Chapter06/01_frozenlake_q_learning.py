4a359055c69cace6d45112bca28ec0768a2d576d
labintsev <andrej.labintsev@yandex.ru>
2024-03-10 13:57:30 +0300

    update plan and sem 5 code

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 """
-Q-learning - modification of the value iteration method:
+Tabular Q-learning - modification of the value iteration method:
 1. Start with an empty table, mapping states to values of actions.
-2. By interacting with the environment, obtain the tuple (s, a, r, s').
+2. By interacting with the environment, sample the tuple (s, a, r, s').
 3. Update the Q(s, a) value using the Bellman approximation:
 Q(s, a) ← r + γ max Q(s′ , a′)
 4. Repeat from step 2.
@@ -17,7 +16,7 @@ ENV_NAME = "FrozenLake-v1"
 GAMMA = 0.9
 ALPHA = 0.2
 TEST_EPISODES = 20
-
+EXPERIMENT_NAME = "-q-learning-determined"
 
 class Agent:
     def __init__(self):
@@ -36,6 +35,7 @@ class Agent:
         return old_state, action, reward, new_state
 
     def best_value_and_action(self, state):
+        """ Select the action with the largest value from the Q-table. """
         best_value, best_action = None, None
         for action in range(self.env.action_space.n):
             action_value = self.Q_values[(state, action)]
@@ -45,16 +45,20 @@ class Agent:
         return best_value, best_action
 
     def value_update(self, s, a, r, next_s):
+        """ Update Q-values table using one step from the environment.
+        Averaging between old and new values of Q using learning rate αlpha. """
         best_v, _ = self.best_value_and_action(next_s)
         new_v = r + GAMMA * best_v
         old_v = self.Q_values[(s, a)]
         self.Q_values[(s, a)] = old_v * (1 - ALPHA) + new_v * ALPHA
 
     def play_episode(self, env):
+        """ Evaluate current policy to check the progress of learning.
+        TODO: random action if Q_value == 0 """
         total_reward = 0.0
         state, _ = env.reset()
         while True:
-            _, action = self.best_value_and_action(state)
+            q_value, action = self.best_value_and_action(state)
             new_state, reward, terminated, truncated, _ = env.step(action)
             total_reward += reward
             if terminated or truncated:
@@ -66,7 +70,7 @@ class Agent:
 if __name__ == "__main__":
     test_env = gym.make(ENV_NAME)
     agent = Agent()
-    writer = SummaryWriter(comment="-q-learning")
+    writer = SummaryWriter(comment=EXPERIMENT_NAME)
 
     iter_no = 0
     best_reward = 0.0
